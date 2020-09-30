b887b04c21e2d5661eb81c28c38d81494a3b964d
Alejandro Martinez <alejomartinez8@gmail.com>
2020-09-29 23:39:37 -0500

    Configure production enviorment

--- src/index.js
+++ src/index.js
@@ -10,46 +10,29 @@ import "./index.css";
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-axios.interceptors.request.use(
-  (req) => {
-    console.log("req: ", req);
-    return req;
-  },
-  (error) => {
-    console.log(error.toJSON());
-    return Promise.reject(error);
-  }
-);
+if (process.env.NODE_ENV === "development") {
+  axios.interceptors.request.use(
+    (req) => {
+      console.log("req: ", req);
+      return req;
+    },
+    (error) => {
+      console.log(error.toJSON());
+      return Promise.reject(error);
+    }
+  );
 
-axios.interceptors.response.use(
-  (res) => {
-    console.log("res:", res);
-    return res;
-  },
-  (error) => {
-    console.log(error.toJSON());
-    return Promise.reject(error);
-  }
-);
-
-// const handleError = (error) => {
-//   if (error.response) {
-//     // The request was made and the server responded with a status code
-//     // that falls out of the range of 2xx
-//     console.log(error.response.data);
-//     console.log(error.response.status);
-//     console.log(error.response.headers);
-//   } else if (error.request) {
-//     // The request was made but no response was received
-//     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-//     // http.ClientRequest in node.js
-//     console.log(error.request);
-//   } else {
-//     // Something happened in setting up the request that triggered an Error
-//     console.log("Error", error.message);
-//   }
-//   console.log(error.config);
-// };
+  axios.interceptors.response.use(
+    (res) => {
+      console.log("res:", res);
+      return res;
+    },
+    (error) => {
+      console.log(error.toJSON());
+      return Promise.reject(error);
+    }
+  );
+}
 
 ReactDOM.render(
   <Provider store={store}>
